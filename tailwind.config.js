df48a3d9e11c51ed4e7044dbca0f375430671cec
louiskop <louisdj12@gmail.com>
2023-10-11 09:07:04 +0200

    converted rem units to vh, windows display magic numbers :)

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,18 @@ module.exports = {
     theme: {
         extend: {
             spacing: {
-                128: "32rem",
+                128: "71.6083916vh",
                 perc: "20%",
             },
+            fontSize: {
+                "sm": ["1.95804vh", "2.797202vh"],
+                "base":["2.23776vh","3.35664vh"],
+                "lg": ["2.5174vh", "3.91608vh"],
+                "xl": ["2.7972vh",  "3.9160vh"],
+                "2xl": ["3.35664vh", "4.4755vh"],
+                "5xl": ["6.71328vh", "1"],
+                "7xl": ["10.0699vh", "1"]
+            }
         },
     },
     plugins: [require("daisyui")],
