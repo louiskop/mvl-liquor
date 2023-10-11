e98a19a93f43d17e2cc38065e831c5c450592b4b
De Jager, L, Mr [24908134@sun.ac.za] <24908134@sun.ac.za>
2023-10-11 08:39:25 +0200

    content changes and vw+vh script added

--- src/styles/animate.js
+++ src/styles/animate.js
@@ -28,3 +28,45 @@ document.querySelectorAll(".left-enter").forEach((el) => {
 document.querySelectorAll(".right-enter").forEach((el) => {
     observer.observe(el);
 });
+
+// === convert rems to viewport units ===
+var viewHeight = window.innerHeight;
+var viewWidth = window.innerWidth;
+
+// Assume default font-size is 16px
+console.log(
+    `text-xl: ${((1.25 * 16) / viewHeight) * 100}vh, lineHeight: ${
+        ((1.75 * 16) / viewHeight) * 100
+    } vh`
+);
+console.log(
+    `text-lg: ${((1.125 * 16) / viewHeight) * 100} vh, lineHeight: ${
+        ((1.75 * 16) / viewHeight) * 100
+    }vh`
+);
+console.log(`text-7xl: ${((4.5 * 16) / viewHeight) * 100}vh , lineHeight:1`);
+
+console.log(
+    `[-4] text-base: ${(16 / viewHeight) * 100}vh , lineHeight:${
+        ((1.5 * 16) / viewHeight) * 100
+    }`
+);
+
+console.log(
+    `text-2xl: ${((16 * 1.5) / viewHeight) * 100}vh , lineHeight: ${
+        ((16 * 2) / viewHeight) * 100
+    }vh`
+);
+
+console.log(
+    `[pt-12] text-5xl: ${((16 * 3) / viewHeight) * 100}vh, lineheight:1`
+);
+console.log(`h-128: ${((16 * 32) / viewHeight) * 100}vh`);
+
+console.log(
+    `text-sm: ${((16 * 0.875) / viewHeight) * 100}vh lineHeight:${
+        ((16 * 1.25) / viewHeight) * 100
+    }`
+);
+
+console.log(`[double for px-24]px-12: ${(16 * 3) / viewWidth}vw - nb vw`);
